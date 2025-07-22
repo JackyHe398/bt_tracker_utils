74bfd4ca2096ffcfd5f226e8afe3a10ea74053fe
JackyHe398 on OldHome_main <hekinghung@gmail.com>
2025-07-22 20:47:35 +0800

    chore: test script

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
-from bt_tracker_utils import CheckTracker, check_trackers, query, TrackerEvent, check_tracker
+from bt_tracker_utils import CheckTracker, check_trackers, query, TrackerEvent, check_tracker, Query
 
-# result = query("udp://tracker.torrent.eu.org:451/announce",
-#           "b547cdccd2f6248e4d76cb3d7a9012cfe3fee2da",
-#           "-robots-testing12345",
-#           TrackerEvent.STOPPED)
+result = query("udp://tracker.torrent.eu.org:451/announce",
+          "b547cdccd2f6248e4d76cb3d7a9012cfe3fee2da",
+          "-robots-testing12345",
+          TrackerEvent.STOPPED)
 
-# print(result)
+print(result)
 
-# result = Query.http("http://tracker.opentrackr.org:1337/announce",
-#           "8a19577fb5f690970ca43a57ff1011ae202244b8",
-#           "-robots-testing12345",
-#           TrackerEvent.STOPPED)
+result = Query.http("http://tracker.opentrackr.org:1337/announce",
+          "8a19577fb5f690970ca43a57ff1011ae202244b8",
+          "-robots-testing12345",
+          TrackerEvent.STOPPED)
 
-# result = query("http://nyaa.tracker.wf:7777/announce",
-#                     "8a19577fb5f690970ca43a57ff1011ae202244b8",
-#                     "-robots-testing12345",
-#                     TrackerEvent.STOPPED)
+result = query("http://nyaa.tracker.wf:7777/announce",
+                    "8a19577fb5f690970ca43a57ff1011ae202244b8",
+                    "-robots-testing12345",
+                    TrackerEvent.STOPPED)
 
-# print(result)
+print(result)
 
-# check_tracker("udp://tracker.torrent.eu.org:451/announce")
+check_tracker("udp://tracker.torrent.eu.org:451/announce")
+
