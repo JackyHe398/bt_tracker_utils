0faa8d315c5107a1fc9a69e3aa4549f6fd286233
JackyHe398 on OldHome_main <hekinghung@gmail.com>
2025-12-26 23:41:12 +0800

    chores: rename subpackage tracker to Tracker

--- bt_tracker_utils/__init__.py
+++ bt_tracker_utils/__init__.py
@@ -1,2 +1,2 @@
-from . import tracker
+from . import Tracker
 from .Torrent import Torrent, TorrentStatus
--- test.py
+++ test.py
@@ -1,33 +1,24 @@
-# from bt_tracker_utils import CheckTracker, check_trackers, TorrentStatus, check_tracker, Query
-
-# result = query("udp://tracker.torrent.eu.org:451/announce",
-#           "b547cdccd2f6248e4d76cb3d7a9012cfe3fee2da",
-#           "-robots-testing12345",
-#           TorrentStatus.STOPPED)
-
-# print(result)
-
-# result = Query.http("http://tracker.opentrackr.org:1337/announce",
-#           "8a19577fb5f690970ca43a57ff1011ae202244b8",
-#           "-robots-testing12345",
-#           TorrentStatus.STOPPED)
-
-# print(result)
-
-# check_tracker("udp://tracker.torrent.eu.org:451/announce")
-
-from bt_tracker_utils import tracker, TorrentStatus, Torrent
+from bt_tracker_utils import Tracker, TorrentStatus, Torrent
 from random import choice, sample
 
+# Test tracker checking
+with open("trackers.txt", "r") as f:
+    urls = list(line.strip() for line in f if line.strip() and not line.startswith("#"))
 
-# with open("trackers.txt", "r") as f:
-#     urls = list(line.strip() for line in f if line.strip() and not line.startswith("#"))
-
-# result = tracker.Check.multiple(urls, timeout=5)
-# print(result)
-
-torrent = Torrent("b547cdccd2f6248e4d76cb3d7a9012cfe3fee2da", 0)
-result = tracker.Query.single(torrent, 'https://tracker.ghostchu-services.top:443/announce',
-            "-robots-testing12345")
-
+result = tracker.Check.multiple(urls, timeout=5)
 print(result)
+
+result = tracker.Check.auto("udp://tracker.torrent.eu.org:451/announce")
+print(result)
+
+# Test tracker querying
+torrent = Torrent("b547cdccd2f6248e4d76cb3d7a9012cfe3fee2da", 0)
+self_peer_id = "-robots-testing12345"
+for tracker_url in ('https://tracker.ghostchu-services.top:443/announce',
+                    'udp://tracker.torrent.eu.org:451/announce',
+                    'http://tracker.opentrackr.org:1337/announce'):
+    try:
+        result =tracker.Query.single(torrent, tracker_url, self_peer_id)
+        print(result)
+    except Exception as e:
+        print(f"Error querying tracker {tracker_url}: {e}")
