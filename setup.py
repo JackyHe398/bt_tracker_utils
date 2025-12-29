8c8e89100372df7ee52940fe4b9f04bd18d68713
JackyHe398 on OldHome_main <hekinghung@gmail.com>
2025-12-29 14:01:20 +0800

    fix(minor): change encoding of readme.md in setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     packages=find_packages(),
     python_requires=">=3.6",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown"
 )
 
